0d2b13217c0babfc58ab95767ec234de0d2dbad6
dede.sunarwan <dede.sunarwan@circlecreative.dev>
2022-05-02 13:25:16 +0700

    class based component

---
+++ src/components/ClassBasedComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ClassBasedComponent extends Component {
+  render() {
+    return <div>Class Based Components</div>;
+  }
+}
+
+export default ClassBasedComponent;
